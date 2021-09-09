fc0a7c9f6ebba55c45bdf7d471d42388b019eee1
GustavoRocha3 <gustavo.rocha3@fatec.sp.gov.br>
2021-09-09 20:46:20 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const cinema = require("./cinema.js");
+
+/* cinema.adicionarFilme(3, "Matrix", 1999, false); */
+
+/* cinema.listarTodosFilmes2();
+
+cinema.buscarFilmePeloCodigo(3); */
+cinema.calcularFaturamento();
